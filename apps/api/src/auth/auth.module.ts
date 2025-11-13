ba72f10753b456b5f167cfd39c8fbbb529f8262a
Preston Asmar <preston.asmar@gmail.com>
2025-11-13 13:43:51 -0600

    Update auth.module.ts

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { UsersModule } from '../users/users.module';
 
@@ -20,6 +21,7 @@ import { UsersModule } from '../users/users.module';
     }),
     UsersModule,
   ],
+  controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
   exports: [AuthService],
 })
